844a10ecd565c1a3b384dd91ce01855365c09fbb
Kenneth Chew <kenneth.c0@protonmail.com>
2022-02-03 17:11:33 -0500

    Set `Content-Type` header

--- ao3_rss/server.py
+++ ao3_rss/server.py
@@ -7,4 +7,6 @@ app = Flask(__name__)
 
 @app.route('/works/<work_id>')
 def work(work_id):
-    return ao3_rss.feed.for_work(work_id)
+    resp = Flask.response_class(ao3_rss.feed.for_work(work_id))
+    resp.headers['Content-Type'] = 'application/atom+xml'
+    return resp
