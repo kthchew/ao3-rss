a0d9e593f0a8ab982c59cc26d719e77dbcb23ef2
Kenneth Chew <kenneth.c0@protonmail.com>
2022-02-03 15:52:31 -0500

    Create Python package

--- ao3_rss/server.py
+++ ao3_rss/server.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-import feed
+import ao3_rss.feed
 
 app = Flask(__name__)
 
 
 @app.route('/works/<work_id>')
 def work(work_id):
-    return feed.for_work(work_id)
+    return ao3_rss.feed.for_work(work_id)
