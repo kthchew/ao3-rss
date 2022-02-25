09e35860fd7086e3bc91b4654f74f700dd2f22e1
Kenneth Chew <kenneth.c0@protonmail.com>
2022-02-25 00:57:38 -0500

    Refactor feed generator to prepare to support regular RSS feeds

--- ao3_rss/feed.py
+++ ao3_rss/feed.py
@@ -1,7 +1,31 @@
 import AO3
 from feedgen.feed import FeedGenerator
+from feedgen.feed import FeedEntry
 from flask import make_response
 
+
+def work_base_feed(work):
+    feed = FeedGenerator()
+    feed.title(work.title)
+    feed.link(href=work.url, rel='alternate')
+    feed.subtitle(work.summary)
+
+    entries = []
+    chapter: AO3.Chapter
+    for chapter in work.chapters[-3:]:
+        entry: FeedEntry = feed.add_entry()
+        entry.id(f"{work.url}/chapters/{chapter.id}")
+        entry.title(f"{chapter.number}. {chapter.title}")
+        entry.link(href=f"{work.url}/chapters/{chapter.id}")
+        formatted_text = ""
+        for text in [chapter.summary, chapter.start_notes, chapter.text, chapter.end_notes]:
+            if text != "":
+                formatted_text += text + '<hr>'
+        # Remove last <hr>
+        entry.content(formatted_text[:-4].replace('\n', '<br><br>'), type='html')
+        entries.append(entry)
+
+    return feed, entries
 
 def work_atom(work_id):
     try:
@@ -10,28 +34,36 @@ def work_atom(work_id):
         return make_response(("Requires authentication", 401))
     except AO3.utils.InvalidIdError:
         return make_response(("No work found", 404))
-    feed = FeedGenerator()
-    feed.id(work.url)
-    feed.title(work.title)
-    feed.author({'name': work.authors[0].username, 'email': work.authors[0].username})
-    feed.link(href=work.url, rel='alternate')
-    feed.subtitle(work.summary)
+    feed, entries = work_base_feed(work)
 
-    chapter: AO3.Chapter
-    for chapter in work.chapters[-3:]:
-        entry = feed.add_entry()
-        entry.id(f"{work.url}/chapters/{chapter.id}")
-        entry.title(f"{chapter.number}. {chapter.title}")
-        entry.link(href=f"{work.url}/chapters/{chapter.id}")
+    feed.id(work.url)
+    feed.author({'name': work.authors[0].username})
+    for entry in entries:
         entry.author({'name': work.authors[0].username})
-        formatted_text = ""
-        for text in [chapter.summary, chapter.start_notes, chapter.text, chapter.end_notes]:
-            if text != "":
-                formatted_text += text + '<hr>'
-        # Remove last <hr>
-        entry.content(formatted_text[:-4].replace('\n', '<br><br>'), type='html')
 
     return feed.atom_str()
+
+def series_base_feed(series):
+    feed = FeedGenerator()
+    feed.title(series.name)
+    feed.link(href=series.url, rel='alternate')
+    feed.subtitle(series.description)
+    
+    entries = []
+    work: AO3.Work
+    for work in series.work_list[-3:]:
+        entry: FeedEntry = feed.add_entry()
+        entry.id(work.url)
+        entry.title(work.title)
+        entry.link(href=work.url)
+        entry.content(work.summary)
+        work.reload(load_chapters=False)
+        # Assume UTC
+        entry.published(str(work.date_published) + '+00:00')
+        entry.updated(str(work.date_updated) + '+00:00')
+        entries.append(entry)
+
+    return feed, entries
 
 def series_atom(series_id):
     try:
@@ -39,25 +71,14 @@ def series_atom(series_id):
     except AO3.utils.AuthError:
         return make_response(("Requires authentication", 401))
     except AO3.utils.InvalidIdError:
-        return make_response(("No work found", 404))
-    feed = FeedGenerator()
+        return make_response(("No series found", 404))
+    feed, entries = series_base_feed(series)
+
     feed.id(series.url)
-    feed.title(series.name)
-    feed.author({'name': series.creators[0].username, 'email': series.creators[0].username})
-    feed.link(href=series.url, rel='alternate')
-    feed.subtitle(series.description)
-    
-    work: AO3.Work
-    for work in series.work_list[-3:]:
-        entry = feed.add_entry()
-        entry.id(work.url)
-        entry.title(work.title)
-        entry.link(href=work.url)
+    feed.author({'name': series.creators[0].username})
+    for entry in entries:
+        work_id = AO3.utils.workid_from_url(entry.id())
+        work = AO3.Work(work_id, load=True, load_chapters=False)
         entry.author({'name': work.authors[0].username})
-        entry.content(work.summary)
-        work.reload(load_chapters=False)
-        # Assume UTC
-        entry.pubDate(str(work.date_published) + '+00:00')
-        entry.updated(str(work.date_updated) + '+00:00')
 
     return feed.atom_str()
