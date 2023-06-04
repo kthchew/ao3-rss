30edbe6c5c0c37f2c010c7cbdffb706730074e13
Kenneth Chew <kenneth.c0@protonmail.com>
2023-06-04 00:49:01 -0400

    Cleanup session code

--- ao3_rss/session.py
+++ ao3_rss/session.py
@@ -5,15 +5,13 @@ import AO3
 
 from ao3_rss import config
 
-_session = None
+_SESSION = None
+if config.USERNAME == '' or config.PASSWORD == '':
+    _SESSION = AO3.GuestSession()
+else:
+    _SESSION = AO3.Session(config.USERNAME, config.PASSWORD)
 
 
 def get_session():
-    """Creates a session with AO3."""
-    global _session
-    if _session is None:
-        if config.USERNAME == '' or config.PASSWORD == '':
-            _session = AO3.GuestSession()
-        else:
-            _session = AO3.Session(config.USERNAME, config.PASSWORD)
-    return _session
+    """Returns a session with AO3. If no username and password are provided, a guest session is used."""
+    return _SESSION
