9c38ceb0232e791dfb2620e6b171edf5ad208bc1
Kenneth Chew <kenneth.c0@protonmail.com>
2023-07-10 19:06:55 -0400

    Create session when needed instead of at startup
    This prevents issues with timeouts when there are problems such as AO3 being down

--- ao3_rss/session.py
+++ ao3_rss/session.py
@@ -11,13 +11,15 @@ import AO3
 from ao3_rss import config
 
 _SESSION = None
-if config.USERNAME != '' and config.PASSWORD != '':
-    try:
-        _SESSION = AO3.Session(config.USERNAME, config.PASSWORD)
-    except TypeError:
-        logging.error('AO3 appears to be down. No session has been created.')
 
 
 def get_session():
     """Returns a session with AO3. If no username and password are provided, no session is returned."""
+    # pylint: disable=global-statement
+    global _SESSION
+    if _SESSION is None and config.USERNAME != '' and config.PASSWORD != '':
+        try:
+            _SESSION = AO3.Session(config.USERNAME, config.PASSWORD)
+        except TypeError:
+            logging.error('AO3 appears to be down. No session has been created.')
     return _SESSION
