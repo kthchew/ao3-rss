19631f4d9e2a4dfbe540404407e1c47834be37ae
Kenneth Chew <kenneth.c0@protonmail.com>
2023-07-11 02:56:14 -0400

    Check time manually for session retry instead of a cache
    The cache did not work with multithreading

--- ao3_rss/session.py
+++ ao3_rss/session.py
@@ -4,25 +4,28 @@
 """
 Provides tools needed to create a session with AO3.
 """
+import datetime
 import logging
 
 import AO3
 import requests.exceptions
-from cachetools import cached, TTLCache
 
 from ao3_rss import config
 
 _SESSION = None
+_last_session_attempt_time = datetime.datetime.min
 
 
-@cached(cache=TTLCache(maxsize=1, ttl=1800))
 def get_session():
     """Returns a session with AO3. If no username and password are provided, no session is returned."""
     # pylint: disable=global-statement
     global _SESSION
-    if _SESSION is None and config.USERNAME != '' and config.PASSWORD != '':
+    global _last_session_attempt_time
+    timediff = datetime.datetime.now() - _last_session_attempt_time
+    if timediff.total_seconds() > 1800 and _SESSION is None and config.USERNAME != '' and config.PASSWORD != '':
         try:
             _SESSION = AO3.Session(config.USERNAME, config.PASSWORD)
         except (TypeError, requests.exceptions.ConnectionError, requests.exceptions.SSLError):
+            _last_session_attempt_time = datetime.datetime.now()
             logging.error('AO3 appears to be down. No session has been created.')
     return _SESSION
