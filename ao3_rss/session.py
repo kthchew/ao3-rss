2457aeabb4ad3da3973afa35dbb44d51974d5972
Kenneth Chew <kenneth.c0@protonmail.com>
2023-07-12 02:00:21 -0400

    Revert "Check time manually for session retry instead of a cache"
    This reverts commit 19631f4d9e2a4dfbe540404407e1c47834be37ae.

--- ao3_rss/session.py
+++ ao3_rss/session.py
@@ -4,32 +4,25 @@
 """
 Provides tools needed to create a session with AO3.
 """
-import datetime
 import logging
-import multiprocessing
 
 import AO3
 import requests.exceptions
+from cachetools import cached, TTLCache
 
 from ao3_rss import config
 
 _SESSION = None
-_last_session_attempt_time = datetime.datetime.min
-_lock = multiprocessing.Lock()
 
 
+@cached(cache=TTLCache(maxsize=1, ttl=1800))
 def get_session():
     """Returns a session with AO3. If no username and password are provided, no session is returned."""
     # pylint: disable=global-statement
     global _SESSION
-    global _last_session_attempt_time
-    _lock.acquire()
-    timediff = datetime.datetime.now() - _last_session_attempt_time
-    if timediff.total_seconds() > 1800 and _SESSION is None and config.USERNAME != '' and config.PASSWORD != '':
+    if _SESSION is None and config.USERNAME != '' and config.PASSWORD != '':
         try:
             _SESSION = AO3.Session(config.USERNAME, config.PASSWORD)
         except (TypeError, requests.exceptions.ConnectionError, requests.exceptions.SSLError):
-            _last_session_attempt_time = datetime.datetime.now()
             logging.error('AO3 appears to be down. No session has been created.')
-    _lock.release()
     return _SESSION
